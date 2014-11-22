bc414b5bdd2ba63c10fae53c8880eb56d072d27e
Devin Rhode <devinrhode2@gmail.com>
2014-11-21 18:19:25 -0800

    mostly copy/paste

---
+++ app/controllers/todo.js
@@ -0,0 +1,26 @@
+import Ember from "ember";
+
+console.log('Todo controller');
+
+export default Ember.ObjectController.extend({
+  isCompleted: function(key, value){
+    var model = this.get('model');
+
+    if (value === undefined) {
+      // property being used as a getter
+      return model.get('isCompleted');
+    } else {
+      // property being used as a setter
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted'),
+  actions: {
+    editTodo: function() {
+      this.set('isEditing', true);
+    }
+  },
+
+  isEditing: false
+});
---
+++ app/controllers/todos.js
@@ -0,0 +1,40 @@
+import Ember from "ember";
+
+console.log('Todos controller');
+
+export default Ember.ArrayController.extend({
+  randomNumber: function(){
+    console.log('computed args',arguments,this);
+    return parseInt(Math.random() * 100);
+  }.property(),
+
+  remaining: function() {
+    return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function() {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining'),
+  
+  actions: {
+    createTodo: function() {
+      // Get the todo title set by the "New Todo" text field
+      var title = this.get('newTitle');
+      if (!title) { return false; }
+      if (!title.trim()) { return; }
+
+      // Create the new Todo model
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false
+      });
+
+      // Clear the "New Todo" text field
+      this.set('newTitle', '');
+
+      // Save the new model
+      todo.save();
+    }
+  }
+});
